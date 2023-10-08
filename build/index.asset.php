dcbed7f4713a055e44b5d53ce235615c3ef4f1a9
San Ha <sanha@MacBook-Air-de-San.local>
2023-10-08 11:37:16 +0200

    Week 5 - add build files

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '324d4ada3acd4448a59b');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'a119a5da84dead319924');
